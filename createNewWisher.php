24de77c626ab68af3294ba4b08a55971d0f242e3
Nam Nguyen Hoai <namnh8x@gmail.com>
2014-04-26 13:36:59 +0200

    Lesson3 half

--- createNewWisher.php
+++ createNewWisher.php
@@ -1,3 +1,33 @@
+<?php
+$dbHost = "sql201.byethost7.com";
+$dbXeHost = "sql201.byethost7.com/XE";
+$dbUsername = "b7_10443524";
+$dbPassword = "ngunhubo";
+
+$UsernameIsUnique = true;
+$passwordIsValid = true;
+$UserIsEmpty = false;
+$passwordIsEmpty = false;
+$password2IsEmpty = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST["user"] == "") {
+        $UserIsEmpty = true;
+    }
+    
+    $con = mysqli_connect($dbHost, $dbUsername, $dbPassword);
+    if (!$con) {
+        exit('Connect Error (' . mysqli_connect_errno() . ') '
+                . mysqli_connect_error());
+    }
+    mysqli_set_charset($con, 'utf-8');
+    
+    mysqli_select_db($con, "b7_10443524_wishlist");
+    mysqli_real_escape_string($con, $_POST["user"]);
+     
+}
+?>
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -10,8 +40,12 @@ and open the template in the editor.
         <title>Create New Wisher</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        Welcome!<br>
+        <form action="createNewWisher.php" method="POST">
+            Your name: <input type="text" name="user"/><br/>
+            Password: <input type="password" name="password"/><br/>
+            Please confirm your password: <input type="password" name="password2"/><br/>
+            <input type="submit" value="Register"/>
+        </form>
     </body>
 </html>
--- index.php
+++ index.php
@@ -13,6 +13,7 @@ and open the template in the editor.
         <form name="wishList" method="GET" action="wishlist.php">
             Show wish list of: <input type="text" name="user" value=""/>
             <input type="submit" value="Go"/>
+            <br>Still don't have a wish list?! <a href="createNewWisher.php">Create now</a>
         </form>
     </body>
 </html>
